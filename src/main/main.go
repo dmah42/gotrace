72f1ed5dda4f4d2f05f5dd7725b52e8cfa87ab1b
Dominic Hamon <dma+github@stripysock.com>
2014-01-05 14:05:09 -0800

    Add scaled sphere to check that works

--- src/main/main.go
+++ src/main/main.go
@@ -7,22 +7,6 @@ const (
 	imageH = 480
 )
 
-func createRandomImage() [][]trace.Color {
-	image := make([][]trace.Color, imageH)
-	for i := range image {
-		image[i] = make([]trace.Color, imageW)
-		for j := range image[i] {
-			image[i][j].R = float64(i) / float64(imageH)
-			image[i][j].G = float64(j) / float64(imageW)
-			image[i][j].B = 0.0
-
-			//log.Printf("%d %d %+v | ", i, j, image[i][j])
-		}
-		//log.Printf("\n")
-	}
-	return image
-}
-
 func main() {
 	c := trace.NewContext(imageW, imageH)
 
@@ -31,10 +15,13 @@ func main() {
 	c.AddPrimitive(trace.NewSphere(o2w))
 
 	o2w = trace.NewM44()
+	o2w.Translate(trace.NewV3(0, 0, -5)).Scale(trace.NewV3(1, 2, 1))
+	c.AddPrimitive(trace.NewSphere(o2w))
+
+	o2w = trace.NewM44()
 	o2w.Translate(trace.NewV3(2, 0, -5))
 	c.AddPrimitive(trace.NewTriangle(o2w))
 
 	image := trace.Render(c)
-	//image := createRandomImage()
 	trace.WriteImageToPPM(image, "render")
 }
