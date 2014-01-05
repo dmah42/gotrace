b0b97f03459b5d96f99735f8abc52e27ad4e9264
Dominic Hamon <dma+github@stripysock.com>
2014-01-04 22:49:16 -0800

    Show sphere and triangle

--- src/main/main.go
+++ src/main/main.go
@@ -27,8 +27,11 @@ func main() {
 	c := trace.NewContext(imageW, imageH)
 
 	o2w := trace.NewM44()
-	o2w.Translate(trace.NewV3(0, 0, -5))/*.scale(trace.NewV3(1.0, 2.0, 1.0))*/
-	//c.AddPrimitive(trace.NewSphere(o2w))
+	o2w.Translate(trace.NewV3(-2, 0, -5))/*.scale(trace.NewV3(1.0, 2.0, 1.0))*/
+	c.AddPrimitive(trace.NewSphere(o2w))
+
+	o2w = trace.NewM44()
+	o2w.Translate(trace.NewV3(2, 0, -5))
 	c.AddPrimitive(trace.NewTriangle(o2w))
 
 	image := trace.Render(c)
