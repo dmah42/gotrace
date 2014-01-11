e82749896d41b22f77bfe31a6adebeb02174010e
Dominic Hamon <dma+github@stripysock.com>
2014-01-11 13:59:46 -0800

    Polymesh now works

--- src/main/main.go
+++ src/main/main.go
@@ -1,33 +1,128 @@
 package main
 
-import "trace"
-
-const (
-	imageW = 640
-	imageH = 480
+import (
+	"math"
+	"os"
+	"strconv"
+	"trace"
 )
 
 var (
-	sphereColor = trace.Color{0, 1, 0.8}
+	sphereColor   = trace.Color{0, 1, 0.8}
 	triangleColor = trace.Color{0.8, 1, 0}
-	lightColor = trace.Color{1, 1, 1}
+	lightColor    = trace.Color{1, 1, 1}
 )
 
+func polySphere(radius float64, divs uint32, o2w *trace.M44, material trace.Material) *trace.PolyMesh {
+	numVertices := (divs-1)*divs + 2
+	P := make([]trace.Pt, numVertices)
+	N := make([]trace.V3, numVertices)
+
+	// vertices
+	u := -math.Pi / 2.0
+	v := -math.Pi
+	du := math.Pi / float64(divs)
+	dv := 2 * math.Pi / float64(divs)
+
+	P[0] = *trace.NewPt(0, -radius, 0)
+	N[0] = *trace.NewV3(0, -radius, 0)
+
+	var i uint32 = 0
+	var k uint32 = 1
+	for ; i < divs-1; i++ {
+		u = u + du
+		v = -math.Pi
+		var j uint32 = 0
+		for ; j < divs; j++ {
+			x := radius * math.Cos(u) * math.Cos(v)
+			y := radius * math.Sin(u)
+			z := radius * math.Cos(u) * math.Sin(v)
+			P[k] = *trace.NewPt(x, y, z)
+			N[k] = *trace.NewV3(x, y, z)
+			v = v + dv
+			k++
+		}
+	}
+	P[k] = *trace.NewPt(0, radius, 0)
+	N[k] = *trace.NewV3(0, radius, 0)
+
+	// polygons
+	npoly := divs * divs
+	nvertices := make([]uint32, npoly)
+	vertices := make([]uint32, (6+(divs-1)*4)*divs)
+
+	// connectivity lists
+	var vid uint32 = 1
+	var numV uint32 = 0
+	var l uint32 = 0
+
+	k = 0
+	for i = 0; i < divs; i++ {
+		var j uint32 = 0
+		for ; j < divs; j++ {
+			if i == 0 {
+				nvertices[k] = 3
+				vertices[l] = 0
+				vertices[l+1] = j + vid
+				if j == divs-1 {
+					vertices[l+2] = vid
+				} else {
+					vertices[l+2] = j + vid + 1
+				}
+			} else if i == divs-1 {
+				nvertices[k] = 3
+				vertices[l] = j + vid + 1 - divs
+				vertices[l+1] = vid + 1
+				if j == divs-1 {
+					vertices[l+2] = vid + 1 - divs
+				} else {
+					vertices[l+2] = j + vid + 2 - divs
+				}
+			} else {
+				nvertices[k] = 4
+				vertices[l] = j + vid + 1 - divs
+				vertices[l+1] = j + vid + 1
+				if j == divs-1 {
+					vertices[l+2] = vid + 1
+					vertices[l+3] = vid + 1 - divs
+				} else {
+					vertices[l+2] = j + vid + 2
+					vertices[l+3] = j + vid + 2 - divs
+				}
+			}
+			l += nvertices[k]
+			k++
+			numV++
+		}
+		vid = numV
+	}
+
+	return trace.NewPolyMesh(o2w, material, npoly, nvertices, vertices, P, N)
+}
+
 func main() {
-	c := trace.NewContext(imageW, imageH)
+	imageW, _ := strconv.ParseUint(os.Args[1], 0, 32)
+	imageH, _ := strconv.ParseUint(os.Args[2], 0, 32)
+
+	c := trace.NewContext(uint32(imageW), uint32(imageH))
 
 	o2w := trace.NewM44()
-	o2w.Translate(trace.NewV3(-2, 0, -5))
-	c.AddPrimitive(trace.NewSphere(o2w, trace.NewSolidColor(sphereColor)))
+	o2w.Translate(trace.NewV3(0, 0, -5))
+	c.AddPrimitive(polySphere(2, 10, o2w, trace.NewSolidColor(sphereColor)))
 
-	o2w = trace.NewM44()
-	o2w.Translate(trace.NewV3(0, 0, -2)).Scale(trace.NewV3(0.2, 0.2, 0.2))
-	c.AddPrimitive(trace.NewSphere(o2w, trace.NewLight(lightColor)))
+	/*
+		o2w := trace.NewM44()
+		o2w.Translate(trace.NewV3(-2, 0, -5))
+		c.AddPrimitive(trace.NewSphere(o2w, trace.NewSolidColor(sphereColor)))
 
-	o2w = trace.NewM44()
-	o2w.Translate(trace.NewV3(2, 0, -5))
-	c.AddPrimitive(trace.NewTriangle(o2w, trace.NewSolidColor(triangleColor)))
+		o2w = trace.NewM44()
+		o2w.Translate(trace.NewV3(0, 0, -2)).Scale(trace.NewV3(0.2, 0.2, 0.2))
+		c.AddPrimitive(trace.NewSphere(o2w, trace.NewLight(lightColor)))
 
+		o2w = trace.NewM44()
+		o2w.Translate(trace.NewV3(2, 0, -5))
+		c.AddPrimitive(trace.NewTriangle(o2w, trace.NewSolidColor(triangleColor)))
+	*/
 	image := trace.Render(c)
 	trace.WriteImageToPPM(image, "render")
 }
--- src/trace/polymesh.go
+++ src/trace/polymesh.go
@@ -1,52 +1,111 @@
 package trace
 
+import "log"
+
 type PolyMesh struct {
-	o2w	M44
-	w2o	M44
-	m	Material
-	tri	[]uint32
-	v	[]Pt
-	n	[]V3
+	o2w  M44
+	w2o  M44
+	m    Material
+	ntri uint32
+	tri  []uint32
+	p    []Pt
+	n    []V3
 }
 
-func NewPolyMesh(o2w *M44, m Material, nv, v []uint32, pts []Pt, norms []V3) *PolyMesh {
-	mesh := &PolyMesh{*o2w, *(o2w.inverse()), m, []uint32{}, []Pt{}, []V3{}}
-	var nt uint32 = 0
+func NewPolyMesh(o2w *M44, m Material, np uint32, nv, v []uint32, pts []Pt, norms []V3) *PolyMesh {
+	mesh := &PolyMesh{*o2w, *(o2w.inverse()), m, 0, []uint32{}, []Pt{}, []V3{}}
 	var faceIndex uint32 = 0
 	var maxVertIndex uint32 = 0
-	for i := range nv {
-		nt = nt + nv[i] - 2
+	var i uint32 = 0
+	for ; i < np; i++ {
+		mesh.ntri = mesh.ntri + nv[i] - 2
 		var j uint32 = 0
 		for ; j < nv[i]; j++ {
-			if v[faceIndex + j] > maxVertIndex {
-				maxVertIndex = v[faceIndex + j]
+			if v[faceIndex+j] > maxVertIndex {
+				maxVertIndex = v[faceIndex+j]
 			}
 		}
 		faceIndex = faceIndex + nv[i]
 	}
-	mesh.v = make([]Pt, maxVertIndex + 1)
-	for i := range mesh.v {
-		mesh.v[i] = *(o2w.transformPt(&(pts[i])))
+	mesh.p = make([]Pt, maxVertIndex+1)
+	for i := range mesh.p {
+		mesh.p[i] = *(o2w.transformPt(&(pts[i])))
+		// TODO: bounding box
 	}
-	mesh.tri = make([]uint32, nt * 3)
-	triIndex := 0
+	log.Printf("Creating polymesh: %d polygons, %d triangles\n", np, mesh.ntri)
+	mesh.tri = make([]uint32, mesh.ntri*3)
 	faceIndex = 0
-	for i := range nv {
+	var triIndex uint32 = 0
+	for i = 0; i < np; i++ {
 		var j uint32 = 0
-		for ; j < nv[i] - 2; j++ {
+		for ; j < nv[i]-2; j++ {
 			mesh.tri[triIndex] = v[faceIndex]
-			mesh.tri[triIndex + 1] = v[faceIndex + j + 1]
-			mesh.tri[triIndex + 2] = v[faceIndex + j + 2]
-			triIndex = triIndex + 3
+			mesh.tri[triIndex+1] = v[faceIndex+j+1]
+			mesh.tri[triIndex+2] = v[faceIndex+j+2]
+			triIndex += 3
 		}
-		faceIndex = faceIndex + nv[i]
+		faceIndex += nv[i]
 	}
 	return mesh
 }
 
+func intersectTriangle(r *Ray, v0, v1, v2 *Pt) (hit bool, t, u, v float64) {
+	e1 := PtDelta(v1, v0)
+	e2 := PtDelta(v2, v0)
+
+	pv := Cross(&r.d, e2)
+	det := Dot(e1, pv)
+	hit = det != 0
+	if !hit {
+		return
+	}
+
+	tv := PtDelta(&r.o, v0)
+	u = Dot(tv, pv) / det
+	hit = u >= 0 && u <= 1
+	if !hit {
+		return
+	}
+
+	qv := Cross(tv, e1)
+	v = Dot(&r.d, qv) / det
+	hit = v >= 0 && u+v <= 1
+	if !hit {
+		return
+	}
+
+	t = Dot(e2, qv) / det
+	hit = true
+	return
+}
+
 func (m *PolyMesh) Intersect(ray *Ray) (hit bool, t, u, v float64) {
-	// TODO
-	return false, 0, 0, 0
+	r := NewRay(m.w2o.transformPt(&ray.o), m.w2o.rotateV3(&ray.d))
+	r.t0 = ray.t0
+	r.t1 = ray.t1
+
+	minT := r.t1
+	minU := 0.0
+	minV := 0.0
+
+	var i uint32 = 0
+	for ; i < m.ntri; i++ {
+		triIndex := i * 3
+		v0 := m.p[m.tri[triIndex]]
+		v1 := m.p[m.tri[triIndex+1]]
+		v2 := m.p[m.tri[triIndex+2]]
+
+		hit, t, u, v := intersectTriangle(r, &v0, &v1, &v2)
+		// NOTE: this will get triangles behind the start of the ray. Should check t vs r.t0
+		if hit && t < minT {
+			minT = t
+			minU = u
+			minV = v
+		}
+	}
+
+	// NOTE: this might discard a triangle that was behind the ray but not include triangles that should have been hit in front
+	return minT < r.t1 && minT > r.t0, minT, minU, minV
 }
 
 func (m *PolyMesh) material() Material {
